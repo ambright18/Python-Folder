1ade0b39aa1c277aea1a2a7018867ac6c89630ce
Alexander Bright <131471830+ambright18@users.noreply.github.com>
2024-06-23 20:29:32 -0400

    Add files via upload

---
+++ madLibs.py
@@ -0,0 +1,9 @@
+#The mose basic and easiest game to code in python, Mad Libs
+
+color = input("Enter a color: ")
+pluralNoun = input("Enter a plural noun: ")
+food = input("Enter a type of food: ")
+
+print("Rose are, ", color)
+print(pluralNoun + " are blue")
+print("I love ", food , ", how about you?")
